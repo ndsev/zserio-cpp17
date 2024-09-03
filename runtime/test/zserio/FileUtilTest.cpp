4eb493dd4d07316b4ad400c22f021ddc18bffd37
Milan Kriz <milan.kriz@eccam.com>
2024-09-03 14:03:47 +0200

    [#1] Add FileUtil from Zserio C++ runtime

---
+++ runtime/test/zserio/FileUtilTest.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "zserio/FileUtil.h"
+
+namespace zserio
+{
+
+TEST(FileUtilTest, writeReadByteBufferBitSize)
+{
+    const std::string fileName = "FileUtilTest_byteBufferBitSize.bin";
+
+    auto buffer = std::vector<uint8_t>{0xAB, 0xCD, 0xF0};
+    writeBufferToFile(buffer.data(), 20, BitsTag(), fileName);
+
+    BitBuffer readBitBuffer = readBufferFromFile(fileName);
+    ASSERT_EQ(24, readBitBuffer.getBitSize());
+    for (size_t i = 0; i < 3; ++i)
+    {
+        ASSERT_EQ(buffer[i], readBitBuffer.getData()[i]);
+    }
+
+    const std::string invalidFileName = "";
+    ASSERT_THROW(writeBufferToFile(buffer.data(), 20, BitsTag(), invalidFileName), CppRuntimeException);
+    ASSERT_THROW(readBufferFromFile(invalidFileName), CppRuntimeException);
+}
+
+TEST(FileUtilTest, writeReadByteBufferByteSize)
+{
+    const std::string fileName = "FileUtilTest_byteBufferByteSize.bin";
+
+    auto buffer = std::vector<uint8_t>{0xAB, 0xCD, 0xF0};
+    writeBufferToFile(buffer.data(), buffer.size(), fileName);
+
+    BitBuffer readBitBuffer = readBufferFromFile(fileName);
+    ASSERT_EQ(24, readBitBuffer.getBitSize());
+    for (size_t i = 0; i < 3; ++i)
+    {
+        ASSERT_EQ(buffer[i], readBitBuffer.getData()[i]);
+    }
+}
+
+TEST(FileUtilTest, writeReadBitBuffer)
+{
+    const std::string fileName = "FileUtilTest_bitBuffer.bin";
+
+    BitBuffer bitBuffer({0xAB, 0xCD, 0xF0}, 20);
+    writeBufferToFile(bitBuffer, fileName);
+
+    BitBuffer readBitBuffer = readBufferFromFile(fileName);
+    ASSERT_EQ(24, readBitBuffer.getBitSize());
+    for (size_t i = 0; i < 3; ++i)
+    {
+        ASSERT_EQ(bitBuffer.getData()[i], readBitBuffer.getData()[i]);
+    }
+}
+
+} // namespace zserio
