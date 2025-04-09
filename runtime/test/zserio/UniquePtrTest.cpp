455793abb8b69bfcee491934371089f0ea5e3931
Milan Kriz <milan.kriz@eccam.com>
2025-04-09 09:41:05 +0200

    Fix UniquePtr to not support polymorphism which can lead to memory leaks
    * add UniquePtrTest

---
+++ runtime/test/zserio/UniquePtrTest.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "zserio/CppRuntimeException.h"
+#include "zserio/UniquePtr.h"
+
+#include "TrackingAllocator.h"
+
+namespace zserio
+{
+
+namespace
+{
+
+struct ThrowingStruct
+{
+    ThrowingStruct()
+    {
+        throw CppRuntimeException("oops");
+    }
+};
+
+} // namespace
+
+TEST(UniquePtrTest, allocateUnique)
+{
+    TrackingAllocator<int> alloc;
+
+    UniquePtr<int, TrackingAllocator<int>> unique = allocate_unique<int>(alloc, 12345);
+    ASSERT_TRUE(bool(unique));
+    ASSERT_EQ(12345, *unique);
+    ASSERT_EQ(1, alloc.numAllocs());
+
+    ASSERT_THROW(allocate_unique<ThrowingStruct>(alloc), CppRuntimeException);
+}
+
+} // namespace zserio
