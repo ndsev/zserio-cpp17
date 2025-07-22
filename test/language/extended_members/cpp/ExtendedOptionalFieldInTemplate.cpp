6729af6d6ac56d27d3ae58704e261eee12057d18
Tomas Pecholt <tomas.pecholt@eccam.com>
2025-07-22 10:58:43 +0800

    [#6] Add extended optional template test

---
+++ test/language/extended_members/cpp/ExtendedOptionalFieldInTemplate.cpp
@@ -0,0 +1,28 @@
+#include "extended_members/extended_optional_field_in_template/ExtendedCompound.h"
+#include "extended_members/extended_optional_field_in_template/ExtendedSimple.h"
+#include "gtest/gtest.h"
+#include "test_utils/TestUtility.h"
+
+namespace extended_members
+{
+namespace extended_optional_field_in_template
+{
+
+TEST(ExtendedOptionalFieldInTemplateTest, writeReadTest)
+{
+    ExtendedSimple data;
+    test_utils::writeReadTest(data);
+    ExtendedCompound data2;
+    test_utils::writeReadTest(data2);
+}
+
+TEST(ExtendedOptionalFieldInTemplateTest, hashTest)
+{
+    ExtendedSimple data;
+    ASSERT_EQ(std::hash<ExtendedSimple>()(data), static_cast<size_t>(851));
+    ExtendedCompound data2;
+    ASSERT_EQ(std::hash<ExtendedCompound>()(data2), static_cast<size_t>(851));
+}
+
+} // namespace extended_optional_field_in_template
+} // namespace extended_members
