e80fa613b7bd25a2b157bfcda5407ca621b8d069
Tomas Pecholt <tomas.pecholt@eccam.com>
2025-07-30 19:46:37 +0800

    [#6] Update functions, extended_members tests

--- test/language/extended_members/cpp/ExtendedOptionalFieldInTemplateTest.cpp
+++ test/language/extended_members/cpp/ExtendedOptionalFieldInTemplateTest.cpp
@@ -1,5 +1,6 @@
 #include "extended_members/extended_optional_field_in_template/ExtendedCompound.h"
 #include "extended_members/extended_optional_field_in_template/ExtendedSimple.h"
+#include "extended_members/extended_optional_field_in_template/Original.h"
 #include "gtest/gtest.h"
 #include "test_utils/TestUtility.h"
 
@@ -12,16 +13,49 @@ TEST(ExtendedOptionalFieldInTemplateTest, writeRead)
 {
     ExtendedSimple data;
     test_utils::writeReadTest(data);
-    ExtendedCompound data2;
-    test_utils::writeReadTest(data2);
+
+    ExtendedCompound extended;
+    extended.extendedValue.setPresent(true);
+    extended.extendedValue.value() = Compound<zserio::UInt32>(20);
+    ASSERT_TRUE(extended.extendedValue.isPresent());
+    ASSERT_EQ((*extended.extendedValue)->field, zserio::UInt32(20));
+    test_utils::writeReadTest(extended);
 }
 
 TEST(ExtendedOptionalFieldInTemplateTest, hash)
 {
-    ExtendedSimple data;
-    ASSERT_EQ(std::hash<ExtendedSimple>()(data), static_cast<size_t>(851));
-    ExtendedCompound data2;
-    ASSERT_EQ(std::hash<ExtendedCompound>()(data2), static_cast<size_t>(851));
+    Original orig(100);
+    ASSERT_EQ(std::hash<Original>()(orig), static_cast<size_t>(951));
+    ExtendedSimple simple(100, {});
+    ASSERT_EQ(std::hash<ExtendedSimple>()(simple), std::hash<Original>()(orig));
+
+    ExtendedCompound extended;
+    extended.extendedValue.setPresent(true);
+    extended.extendedValue.value() = Compound<zserio::UInt32>(20);
+    ASSERT_EQ(std::hash<ExtendedCompound>()(extended), static_cast<size_t>(32358));
+}
+
+TEST(ExtendedOptionalFieldTemplateTest, writeOriginalReadExtended)
+{
+    Original orig(27);
+    auto origView = zserio::View(orig);
+    auto bitBuffer = zserio::serialize(orig);
+
+    ExtendedCompound extended;
+    auto extendedView = zserio::deserialize<ExtendedCompound>(bitBuffer, extended);
+    ASSERT_EQ(extended.value, orig.value);
+    ASSERT_FALSE(extended.extendedValue.isPresent());
+    ASSERT_EQ(zserio::detail::bitSizeOf(extendedView), zserio::detail::bitSizeOf(origView));
+}
+
+TEST(ExtendedOptionalFieldTemplateTest, writeExtendedReadOriginal)
+{
+    ExtendedCompound extended(100, {});
+    auto bitBuffer = zserio::serialize(extended);
+
+    Original orig;
+    zserio::deserialize<Original>(bitBuffer, orig);
+    ASSERT_EQ(orig.value, extended.value);
 }
 
 } // namespace extended_optional_field_in_template
--- test/language/functions/cpp/TemplatedStructureParentChildValueTest.cpp
+++ test/language/functions/cpp/TemplatedStructureParentChildValueTest.cpp
@@ -20,5 +20,17 @@ TEST(TemplatedStructureParentChildValueTest, hash)
     ASSERT_EQ(std::hash<TemplatedStructureParentChildValue>()(data), static_cast<size_t>(2553));
 }
 
+TEST(TemplatedStructureParentChildValueTest, checkFunctions)
+{
+    TemplatedStructureParentChildValue data;
+    data.parentValue.childValue.val = 26;
+
+    zserio::View childView(data.parentValue.childValue);
+    ASSERT_EQ(childView.getValue(), data.parentValue.childValue.val);
+
+    zserio::View parentView(data.parentValue);
+    ASSERT_EQ(parentView.getValue(), childView.getValue());
+}
+
 } // namespace templated_structure_parent_child_value
 } // namespace functions
