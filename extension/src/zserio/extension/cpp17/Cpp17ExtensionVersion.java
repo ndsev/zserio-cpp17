9ae580e97c7effea5b8cf55626582d04a0339500
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2025-07-31 14:36:28 +0200

    Set extension version to 0.5.0

--- extension/src/zserio/extension/cpp17/Cpp17ExtensionVersion.java
+++ extension/src/zserio/extension/cpp17/Cpp17ExtensionVersion.java
@@ -10,7 +10,7 @@ package zserio.extension.cpp17;
 public final class Cpp17ExtensionVersion
 {
     /** C++ extension version string. */
-    public static final String CPP17_EXTENSION_VERSION_STRING = "0.4.0";
+    public static final String CPP17_EXTENSION_VERSION_STRING = "0.5.0";
 
     /** Expected Zserio core version string. */
     public static final String EXPECTED_ZSERIO_VERSION_STRING = "2.17.0";
@@ -22,5 +22,5 @@ public final class Cpp17ExtensionVersion
     public static final String JSON_VERSION_STRING = "1.0";
 
     /** C++ extension version in integer value to allow version checking in generated code. */
-    public static final long CPP17_EXTENSION_VERSION_NUMBER = 4000;
+    public static final long CPP17_EXTENSION_VERSION_NUMBER = 5000;
 }
