84bb40144738567c46a576101ad2201cd59f8de1
Tomas Pecholt <tomas.pecholt@eccam.com>
2025-11-27 19:03:04 +0100

    Improve SQLite cmake, add options to build without or with custom SQLite

--- cmake/sqlite_utils.cmake
+++ cmake/sqlite_utils.cmake
@@ -1,12 +1,7 @@
 include(compiler_utils)
 
 # A function to add SQLite library target
-function(sqlite_add_library PROJECT_ROOT)
-    set(SQLITE_ROOT ${PROJECT_ROOT}/3rdparty/cpp/sqlite3)
-
+function(sqlite_add_library SQLITE_ROOT)
     # add sqlite library
     add_subdirectory(${SQLITE_ROOT} sqlite3)
-
-    set(SQLITE_LIBRARY SQLite3 PARENT_SCOPE)
-    set(SQLITE_INCDIR ${SQLITE_ROOT} PARENT_SCOPE)
 endfunction()
