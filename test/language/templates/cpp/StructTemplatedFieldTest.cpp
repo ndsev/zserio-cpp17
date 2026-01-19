1bb201a78f35b62fa780f5d73ca84d8aa1efef29
Tomas Pecholt <tomas.pecholt@eccam.com>
2026-01-19 17:49:41 +0700

    [#104] Improve test framework to catch the missing zserioData

--- test/language/templates/cpp/StructTemplatedFieldTest.cpp
+++ test/language/templates/cpp/StructTemplatedFieldTest.cpp
@@ -21,5 +21,31 @@ TEST(StructTemplatedFieldTest, writeRead)
     test_utils::writeReadTest(data);
 }
 
+TEST(StructTemplatedFieldTest, methods)
+{
+    Field<StringType> data1;
+    Field<StringType> data2;
+    data2.value = "different";
+    ASSERT_FALSE(data1 == data2);
+    ASSERT_TRUE(data1 != data2);
+    ASSERT_TRUE(data1 < data2);
+    ASSERT_TRUE(data1 <= data2);
+    ASSERT_FALSE(data1 > data2);
+    ASSERT_FALSE(data1 >= data2);
+    ASSERT_EQ(std::hash<decltype(data1)>()(data1), 23);
+
+    zserio::View<Field<StringType>> vi1(data1);
+    zserio::View<Field<StringType>> vi2(data2);
+    ASSERT_FALSE(vi1 == vi2);
+    ASSERT_TRUE(vi1 != vi2);
+    ASSERT_TRUE(vi1 < vi2);
+    ASSERT_TRUE(vi1 <= vi2);
+    ASSERT_FALSE(vi1 > vi2);
+    ASSERT_FALSE(vi1 >= vi2);
+
+    ASSERT_EQ(&vi1.zserioData(), &data1);
+    ASSERT_EQ(std::hash<decltype(vi1)>()(vi1), 23);
+}
+
 } // namespace struct_templated_field
 } // namespace templates
