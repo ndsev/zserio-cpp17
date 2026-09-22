5d7f778b3ffa0a8a20701d6800121ec8ac386065
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2026-09-22 13:16:57 +0200

    Set extension version to 1.2.1

--- runtime/src/zserio/CppRuntimeVersion.h
+++ runtime/src/zserio/CppRuntimeVersion.h
@@ -2,7 +2,7 @@
 #define ZSERIO_CPP_RUNTIME_VERSION_H_INC
 
 /** C++ extension runtime version string. */
-constexpr const char* CPP17_EXTENSION_RUNTIME_VERSION_STRING = "1.2.0";
+constexpr const char* CPP17_EXTENSION_RUNTIME_VERSION_STRING = "1.2.1";
 
 /** C++ extension runtime version in integer value to allow version checking in generated code. */
 #define CPP17_EXTENSION_RUNTIME_VERSION_NUMBER 1002000
